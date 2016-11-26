6a3e37d132a902cc474256a9a33608f812bf9df9
Maksym Skliarov <maksym.skliarov@gmail.com>
2016-11-26 11:57:56 +0200

    Removed show and update actions for ArticleImages

--- app/controllers/api/react/article_images_controller.rb
+++ app/controllers/api/react/article_images_controller.rb
@@ -7,28 +7,10 @@ class Api::React::ArticleImagesController < Api::React::ApiController
     render json: article_images_json, status: :ok
   end
   
-  # GET api/react/article_images/:id
-  def show
-    article_image = ArticleImage.find(params[:id])
-    article_image_json = ArticleImageSerializer.new(article_image).attributes.as_json
-    render json: article_image_json, status: :ok
-  end
-  
   # POST api/react/article_images
   def create
     article_image = ArticleImage.new(article_images_params)
     if article_image.save
-      article_image_json = ArticleImageSerializer.new(article_image).attributes.as_json
-      render json: article_image_json, status: :ok
-    else
-      render json: { errors: article_image.errors.full_messages }, status: :bad_request
-    end
-  end
-  
-  # PUT/PATCH api/react/article_images/:id
-  def update
-    article_image = ArticleImage.find(params[:id])
-    if article_image.update(article_images_params)
       article_image_json = ArticleImageSerializer.new(article_image).attributes.as_json
       render json: article_image_json, status: :ok
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :react do
       resources :articles, only: [:index, :show, :create, :update, :destroy]
-      resources :article_images, only: [:index, :show, :create, :update, :destroy]
+      resources :article_images, only: [:index, :create, :destroy]
     end
   end
 end

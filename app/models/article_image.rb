174f4c5e5fdaea0c9d49a3e4ab2473f172520216
Maksym Skliarov <maksym.skliarov@gmail.com>
2016-11-26 15:44:19 +0200

    Remove empty directory after ArticleImage is deleted

--- app/models/article_image.rb
+++ app/models/article_image.rb
@@ -1,3 +1,17 @@
 class ArticleImage < ApplicationRecord
   mount_uploader :image, ArticleImageUploader
+  
+  # Delete empty directory when ArticleImage is deleted
+  before_destroy :remember_id
+  after_destroy :remove_empty_directory
+  
+  protected
+  
+    def remember_id
+      @id = id
+    end
+    
+    def remove_empty_directory
+      FileUtils.remove_dir("#{Rails.root}/public/uploads/article_images/#{@id}", force: true)
+    end
 end

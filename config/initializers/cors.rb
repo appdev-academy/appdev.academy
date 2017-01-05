75f58a298b47effad7697ef19c689233b872a289
Maksym Skliarov <maksym.skliarov@gmail.com>
2017-01-05 15:28:17 +0200

    Updated CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://www.appdev.academy'
+    origins 'localhost:3000', 'https://admin.appdev.academy'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]

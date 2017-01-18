81017a3bf56f0d79a2b2b7117ae0d5bc64ee2e07
Maksym Skliarov <maksym.skliarov@gmail.com>
2017-01-18 22:24:07 +0200

    Lessons

--- app/serializers/lesson_index_serializer.rb
+++ app/serializers/lesson_index_serializer.rb
@@ -1,3 +1,7 @@
 class LessonIndexSerializer < ActiveModel::Serializer
-  attributes :id, :is_hidden, :position, :slug, :title
+  attributes :id, :is_hidden, :position, :screencast_id, :slug, :title, :topic_id
+  
+  def topic_id
+    object.screencast.topic_id
+  end
 end

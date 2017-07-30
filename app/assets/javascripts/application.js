fde75cee9f4da74b68bd70c259c0b06374eb9ebc
Maksym Skliarov <maksym.skliarov@gmail.com>
2017-07-30 19:35:19 +0300

    Mobile menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require social-share-button
+//= require photoswipe
 //= require_tree .
+
+function toggleMenu() {
+  var menu = document.getElementById('menu');
+  if (menu.className === 'menu') {
+      menu.className += ' responsive';
+  } else {
+      menu.className = 'menu';
+  }
+}

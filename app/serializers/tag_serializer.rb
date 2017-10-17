10e5d1d6f2a17fad80bbb9cc62503925efccd39a
Maksym Skliarov <maksym.skliarov@gmail.com>
2017-10-17 09:14:43 +0300

    Added articles and projects counters to TagSerializer

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,3 +1,15 @@
 class TagSerializer < ActiveModel::Serializer
-  attributes :id, :slug, :title
+  attribute :id
+  attribute :slug
+  attribute :title
+  
+  attribute :articles_count
+  def articles_count
+    object.articles.count
+  end
+  
+  attribute :projects_count
+  def projects_count
+    object.projects.count
+  end
 end

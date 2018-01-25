46a6d084314ac50de45a67e342faeb1a3d79ef50
Denis Metelev <denis@appdev.academy>
2018-01-25 13:59:27 +0200

    Update serializer

--- app/serializers/employee_index_serializer.rb
+++ app/serializers/employee_index_serializer.rb
@@ -1,10 +1,11 @@
 class EmployeeIndexSerializer < ActiveModel::Serializer
   attribute :first_name
+  attribute :id
   attribute :last_name
   
   attribute :profile_picture
   def profile_picture
-    object.profile_picture.thumb
+    object.profile_picture.thumb.url
   end
   
   attribute :position
--- app/serializers/employee_show_serializer.rb
+++ app/serializers/employee_show_serializer.rb
@@ -2,7 +2,12 @@ class EmployeeShowSerializer < ActiveModel::Serializer
   attribute :first_name
   attribute :id
   attribute :last_name
+  
   attribute :profile_picture
+  def profile_picture
+    object.profile_picture.thumb.url
+  end
+  
   attribute :position
   attribute :published
   attribute :title

f26ae11e44daf055698a02903528b0b8a7dcc771
Maksym Skliarov <maksym.skliarov@gmail.com>
2018-08-29 21:30:52 +0300

    Ran react_on_rails installation scripts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+config.webpacker.check_yarn_integrity = true
+
   # Asset host domain
   config.action_controller.asset_host = 'http://localhost:3001'
   

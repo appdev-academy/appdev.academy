ab73b7ffbc18b84ba43567c2fa968fed1617935b
Maksym Skliarov <maksym.skliarov@gmail.com>
2018-09-14 10:08:09 +0300

    Added yarn command to deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,6 +20,9 @@ ssh -p $SSH_PORT root@$HOST << SCRIPT
   # Update bundle
   RAILS_ENV=production bundle install
   
+  # Install yarn packages
+  RAILS_ENV=production yarn
+  
   # Run migrations and seed database
   RAILS_ENV=production rake db:migrate
   RAILS_ENV=production rake db:seed

32d9239bcbc15972e1aff48f60587dd2c7efde9d
Denys Metelov <denis@appdev.academy>
2019-05-14 14:41:58 +0300

    Fix test generator

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module AppDev
     # Disable automatic generation for TestUnit, JS, CSS files and helpers
     config.generators do |g|
       g.test_framework :rspec, views: false, fixture: true
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
       g.stylesheets false
       g.javascripts false
       g.helper false

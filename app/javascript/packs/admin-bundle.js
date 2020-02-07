77924f48966dd02fb8ff42e8083d97edf5573ee7
Skumring <alatelb@gmail.com>
2020-02-07 10:18:43 +0200

    Properly included all part of react-summernote

--- app/javascript/packs/admin-bundle.js
+++ app/javascript/packs/admin-bundle.js
@@ -1,5 +1,8 @@
 import ReactOnRails from 'react-on-rails'
 
+// Import entry stylesheets for pack - IMPORTANT!
+import 'stylesheets/admin-bundle'
+
 import AdminApp from '../bundles/Admin/components/App'
 
 // This is how react_on_rails can see the HelloWorld in the browser.
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
+const loaders = require('./loaders');
+
+// Require loaders
+environment.loaders.append('CSSLoaders', loaders.CSSLoaders);
 
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
     $: 'jquery',

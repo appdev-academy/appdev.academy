2b1a832a26c660faf5adad910c8b9a3a1b815be1
Skumring <alatelb@gmail.com>
2020-02-11 09:11:33 +0200

    Worked solution for properly integrate resolve-url-loader + plugin summernote

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,10 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
-const loaders = require('./loaders');
 
-// Require loaders
-environment.loaders.append('CSSLoaders', loaders.CSSLoaders);
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+});
 
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
